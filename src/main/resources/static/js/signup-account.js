d1efaddf68081045bf30559e128913faaa0ec028
duncanwhyte <duncan_whyte@hotmail.co.uk>
2023-07-22 13:49:39 +0100

    Added an extra li element to let user know passwords must match. Also inplemented a password check to make sure both password inputs are the same. Finally if passwords match the function will also convert user data to an object

---
+++ src/main/resources/static/js/signup-account.js
@@ -0,0 +1,28 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault()
+    const validInputValues = checkSignupValidity()
+    if (validInputValues) {
+        /// Create the new user in database ??
+    } else {
+        /// Show message that input values are not correct
+    }
+
+
+})
+function checkSignupValidity() {
+    const firstName = document.getElementById("name-input")
+    const userName = document.getElementById("username-input")
+    const newPassword = document.getElementById("pwd")
+    const repeatPassword = document.getElementById("repeat-pwd")
+    if (newPassword.value !== repeatPassword.value) {
+        newPassword.setCustomValidity("Passwords do not match.")
+        repeatPassword.setCustomValidity("Passwords do not match.")
+    } else {
+        return {
+            firstName: firstName.value,
+            userName: userName.value,
+            repeatPassword: repeatPassword.value
+        }
+    }
+
+}
